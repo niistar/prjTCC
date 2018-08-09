1c08c2dcbb451ab2a074c030ef0465a47ed7c3b9
Gabriel N. Bourg <gbourg@dcc.local>
2018-08-09 18:06:40 -0300

    Página de login

--- prjNodeAng/app/routes/routes.js
+++ prjNodeAng/app/routes/routes.js
@@ -176,7 +176,7 @@ module.exports = function(app, passport) {
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
+        res.render('signup.ejs', { message: req.flash('signupMessage'), user: 'tes' });
     });
 
     // process the signup form
