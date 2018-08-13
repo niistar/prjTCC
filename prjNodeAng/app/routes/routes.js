9f60d646e81fe8156fe81a54a15f9a4674ff7c21
Gabriel N. Bourg <gbourg@dcc.local>
2018-08-13 18:01:47 -0300

    Página de perfil

--- prjNodeAng/app/routes/routes.js
+++ prjNodeAng/app/routes/routes.js
@@ -159,7 +159,7 @@ module.exports = function(app, passport) {
     app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+        res.render('login.ejs', { message: req.flash('loginMessage'), user: 'tes'  }); 
     });
 
     // process the login form
